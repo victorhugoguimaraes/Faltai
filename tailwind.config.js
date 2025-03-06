09135bbea2c868b3787aa84607b2a4e13ff96421
Victor Guimarães <victorguinascimento@gmail.com>
2025-03-06 11:42:28 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Adapte conforme necessário
+    "./src/**/*.{js,ts,jsx,tsx}", 
   ],
   theme: {
     extend: {},
